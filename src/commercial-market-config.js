07035a2c2ed293d40751cb0aca7bf8b5973fd5b8
Rastty <pgalik79@gmail.com>
2026-09-01 07:27:45 +0200

    Cover all seven markets in commercial audits

---
+++ src/commercial-market-config.js
@@ -0,0 +1,9 @@
+export const COMMERCIAL_MARKET_CONFIG = Object.freeze([
+  Object.freeze({ market: "cs-CZ", locale: "cs", files: Object.freeze(["data/products.json", "data/products-ampul-cz.json"]) }),
+  Object.freeze({ market: "sk-SK", locale: "sk", files: Object.freeze(["data/products-sk.json"]) }),
+  Object.freeze({ market: "pl-PL", locale: "pl", files: Object.freeze(["data/products-pl.json"]) }),
+  Object.freeze({ market: "hu-HU", locale: "hu", files: Object.freeze(["data/products-hu.json"]) }),
+  Object.freeze({ market: "pt-PT", locale: "pt", files: Object.freeze(["data/products-pt.json"]) }),
+  Object.freeze({ market: "ro-RO", locale: "ro", files: Object.freeze(["data/products-ro.json"]) }),
+  Object.freeze({ market: "sl-SI", locale: "sl", files: Object.freeze(["data/products-si.json"]) }),
+]);
